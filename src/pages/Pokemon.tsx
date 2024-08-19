1b77b9eff808ad88b8fc80325c3d16566f5f8197
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-08-19 22:35:18 +0200

    update page Pokemon

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,9 +1,11 @@
+import Header from "../components/header/Header"
+
 export default function Pokemon() {
 
     return (
-      <>
+      <div>
+        <Header />
         <p>Page Pokémon</p>
-      </>
+      </div>
     )
-  }
-  
+}
